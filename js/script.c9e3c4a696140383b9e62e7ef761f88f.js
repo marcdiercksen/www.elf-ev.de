768a83adc7d8f83d41e420f02e814b00a5d6dbeb
Michel Zimmer <mzimmer@uni-bremen.de>
2018-04-06 16:40:51 +0200

    Automate calendar rendering

---
+++ js/script.c9e3c4a696140383b9e62e7ef761f88f.js
@@ -0,0 +1,55 @@
+window.replaceWithCalendar = function (calendarPlaceholder, calendarData) {
+  var ul = document.createElement('ul')
+  {
+    ul.classList.add('mdl-list')
+    var i = 0, n = calendarData.events.length
+    while (i < n) {
+      var event = calendarData.events[i]
+      var li = document.createElement('li')
+      {
+        li.classList.add('mdl-list__item')
+        li.classList.add('mdl-list__item--two-line')
+        var primaryContentSpan = document.createElement('span')
+        {
+          primaryContentSpan.classList.add('mdl-list__item-primary-content')
+          var iconI = document.createElement('i')
+          {
+            iconI.classList.add('material-icons')
+            iconI.classList.add('mdl-list__item-icon')
+            iconI.appendChild(document.createTextNode(event.icon))
+          }
+          primaryContentSpan.appendChild(iconI)
+          var contentSpan = document.createElement('span')
+          {
+            if (event.canceled) contentSpan.classList.add('canceled')
+            contentSpan.appendChild(document.createTextNode(event.title))
+          }
+          primaryContentSpan.appendChild(contentSpan)
+          var subtitleSpan = document.createElement('span')
+          {
+            subtitleSpan.classList.add('mdl-list__item-sub-title')
+            subtitleSpan.appendChild(document.createTextNode(event.date))
+          }
+          primaryContentSpan.appendChild(subtitleSpan)
+        }
+        li.appendChild(primaryContentSpan)
+        if (event.canceled) {
+          var secondaryContentSpan = document.createElement('span')
+          {
+            secondaryContentSpan.classList.add('mdl-list__item-secondary-content')
+            var statusI = document.createElement('i')
+            {
+              statusI.classList.add('material-icons')
+              statusI.appendChild(document.createTextNode('error_outline'))
+            }
+            secondaryContentSpan.appendChild(statusI)
+          }
+          li.appendChild(secondaryContentSpan)
+        }
+      }
+      ul.appendChild(li)
+      i += 1
+    }
+  }
+  calendarPlaceholder.parentNode.replaceChild(ul, calendarPlaceholder)
+}
